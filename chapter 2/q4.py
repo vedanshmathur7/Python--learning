b0b5319e979025de73589ffdee2713823f4feb54
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-21 02:08:54 +0530

    Create q4.py

---
+++ chapter 2/q4.py
@@ -0,0 +1,6 @@
+# Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80
+
+a = int (input("Enter the no. a : ")) 
+b = int (input("Enter the no. b : ")) 
+# b = 80
+print("The statement a is greater than b is :", a>b)
