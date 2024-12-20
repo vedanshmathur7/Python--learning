2c09436478c35a709a366d7353d614c3d0239d10
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-21 02:09:02 +0530

    Create variables.py

---
+++ chapter 2/variables.py
@@ -0,0 +1,20 @@
+a= 5
+b=5 
+c = a+b 
+print (c)
+
+a = 4-3 
+b *= 6
+print (b)
+
+d = 5==6 
+print (d)
+
+print (True or False)
+print (True and False)
+print (True or True)
+print (True and True)
+print (False or False)
+print (False and False)
+
+print (not(False))
