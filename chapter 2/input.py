5488011819480308b1d0c2241307f4b2815556b6
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-21 02:08:42 +0530

    Create input.py

---
+++ chapter 2/input.py
@@ -0,0 +1,9 @@
+a= input("enter no . a : ")
+b= input("enter no . b : ")
+print (a+b)
+
+# prints because takes as the string 
+
+a= int(input("enter no . a : "))
+b= int(input("enter no . b : "))
+print (a+b)
