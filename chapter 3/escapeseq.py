7a1639550555a437cb35cd3872fcc5b9f13804fc
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-21 04:27:42 +0530

    Create escapeseq.py

---
+++ chapter 3/escapeseq.py
@@ -0,0 +1,5 @@
+print("vedansh mathur is a\ngood boy") #  \n prints a new line
+print("vedansh mathur is a\tgood boy") #  \t gives a bar space
+print("vedansh mathur is a \'good\' boy") #  \' prints a single quote
+print("vedansh mathur is a \"good\" boy") #  \" prints a double quote
+print("vedansh mathur is a \\good\\ boy") #  \\ prints a backslash
