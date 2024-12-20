d48cce838bc95aafbf0fb82136095bd7ead9e810
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-21 02:08:24 +0530

    Create first.py

---
+++ Chapter 1/first.py
@@ -0,0 +1,47 @@
+print("Hello world")
+n = 56
+print (n)
+
+import pyjokes
+joke = pyjokes.get_joke()
+print (joke)
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Madarchod behn ke lunnd")
+engine.runAndWait()
+
+# import emoji
+# print(emoji.emojize('Python is :thumbs_up:'))
+# Python is 👍
+# print(emoji.emojize('Python is :thumbsup:', language='alias'))
+# Python is 👍
+# print(emoji.demojize('Python is 👍'))
+# Python is :thumbs_up:
+# print(emoji.emojize("Python is fun :red_heart:"))
+# Python is fun ❤
+# print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
+# Python is fun ❤️ #red heart, not black heart
+# print(emoji.is_emoji("👍"))
+# True
+
+
+# from art import *
+# art_1=art("coffee") # return art as str in normal mode
+# print(art_1)
+# c[_]
+# art_2=art("woman",number=2) # return multiple art as str
+# print(art_2)
+# ▓⚗_⚗▓ ▓⚗_⚗▓ 
+# art("coffee", number=3, space=5) 
+# 'c[_]     c[_]     c[_]'
+# art("random") # random 1-line art mode
+# '(っ◕‿◕)っ '
+# art("rand")   # random 1-line art mode
+# 't(-_-t) 
+# art(22,number=1) # raise artError
+# Traceback (most recent call last):
+#         ...
+# art.art.artError: The 'artname' type must be str.
+
+
