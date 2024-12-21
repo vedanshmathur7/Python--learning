d34fa83f8267652721e2d34b7470e1f65c95497d
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-22 00:33:45 +0530

    Create q2.py

---
+++ chapter 3/q2.py
@@ -0,0 +1,7 @@
+letter = '''Dear <|Name|>,
+You are selected!
+<|Date|> '''
+
+print(letter.replace("<|Name|>", "Vedansh Mathur").replace ("<|Date|>", "21st December, 2024"))
+
+#nested replace function
