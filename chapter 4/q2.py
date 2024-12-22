32459c87b4309c3c12738caae5c54069dbac07ee
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-23 01:37:04 +0530

    Create q2.py

---
+++ chapter 4/q2.py
@@ -0,0 +1,19 @@
+marks = []
+
+f1 = int(input("Enter marks in sub 1 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 2 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 3 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 4 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 5 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 6 : "))
+marks.append(f1)
+f1 = int(input("Enter marks in sub 7 : "))
+marks.append(f1)
+
+marks.sort()
+print(marks)
