cfac7eb45ec3c78b2b40bab7e1a4405d37e5ab74
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-23 01:37:02 +0530

    Create q1.py

---
+++ chapter 4/q1.py
@@ -0,0 +1,18 @@
+fruits = []
+
+f1 = input("Enter fruit 1 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 2 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 3 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 4 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 5 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 6 : ")
+fruits.append(f1)
+f1 = input("Enter fruit 7 : ")
+fruits.append(f1)
+
+print(fruits)
