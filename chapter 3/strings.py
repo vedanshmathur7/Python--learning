4c6973d4ab462eb114b321d5a031594033c58c60
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-23 01:36:55 +0530

    Create strings.py

---
+++ chapter 3/strings.py
@@ -0,0 +1,14 @@
+name = "Vedansh Mathur"
+
+nameshort = name[0:9] # Vedansh M
+print (nameshort)
+nameshort = name[:9] # Vedansh M
+print (nameshort)
+nameshort = name[0:] # Vedansh Mathur
+print (nameshort)
+nameshort = name[0:14] # Vedansh Mathur
+print (nameshort)
+
+
+print (name[0:4]) #Goes till (n-1)th term ex. [0:4] -> Veda
+print (name[10]) #t
