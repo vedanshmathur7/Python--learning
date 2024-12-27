f700b26fb49c7cf77f082f5ab44246609a50f6a3
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:44:20 +0530

    Create q7.py

---
+++ chapter 6/q7.py
@@ -0,0 +1,7 @@
+post = input("Enter the post : ")
+
+if ("vedansh" in post.lower()):
+    print("The post was talking about Vedansh !")
+
+else :
+    print("Nooo , the post was not talking about Vedansh! ")
