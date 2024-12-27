7540b3e5129cd9734904d5068a2b6f076c5dcdf0
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:44:22 +0530

    Create breakstatements.py

---
+++ chapter 7/breakstatements.py
@@ -0,0 +1,19 @@
+for i in range (101): #starts from 0 .... ends till (n-1)
+    if (i==67):
+        break #exits the loop
+    
+    print(i)
+
+
+
+for i in range (0, 100):
+    if (i%11 == 0):
+        
+        print (i)
+
+
+
+for i in range (0,100,25): #(start, stop, step_size)
+    print(i)
+
+    
