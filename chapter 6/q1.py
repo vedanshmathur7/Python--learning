365fa0d2a5e6d69d00968b90c5ed3026e976cb3c
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:43:59 +0530

    Create q1.py

---
+++ chapter 6/q1.py
@@ -0,0 +1,13 @@
+n1 = int (input ("Enter the no. 1 : "))
+n2 = int (input ("Enter the no. 2 : "))
+n3 = int (input ("Enter the no. 3 : "))
+n4 = int (input ("Enter the no. 4 : "))
+
+if (n1>n2 and n1>n3 and n1>n4):
+    print ("No. 1 is the greates among all. ")
+if (n2>n1 and n2>n3 and n2>n4):
+    print ("No. 2 is the greates among all. ")
+if (n3>n2 and n3>n1 and n3>n4):
+    print ("No. 3 is the greates among all. ")
+if (n4>n2 and n4>n3 and n4>n1):
+    print ("No. 4 is the greates among all. ")
