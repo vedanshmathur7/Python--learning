3fa0be9c98b201248c0debea277ed4efeedd3a79
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:44:15 +0530

    Create q6.py

---
+++ chapter 6/q6.py
@@ -0,0 +1,17 @@
+marks = int (input("Enter the marks mf !! : "))
+
+if (marks > 90 and marks <=100):
+    print("The grade is Ex")
+elif (marks > 80 and marks <=90):
+    print("The grade is A")
+elif (marks > 70 and marks <=80):
+    print("The grade is B")
+elif (marks > 60 and marks <=70):
+    print("The grade is C")
+elif (marks > 50 and marks <=60):
+    print("The grade is D")
+elif (marks < 50):
+    print("The grade is F")
+
+if(marks<70):
+    print ("Better luck next time anyways")
