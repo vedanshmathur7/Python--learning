ab54b2c26e4bfdc57d0e0ffb9d0fa68c2af8786b
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:43:56 +0530

    Create multiple_if_statements.py

---
+++ chapter 6/multiple_if_statements.py
@@ -0,0 +1,17 @@
+age = int (input ("Enter your age : "))
+
+if (age % 2 == 0):
+    print ("age is even no.")
+
+else : 
+    print ("age is odd. ")
+
+if (age >= 18):
+    print ("Baccha bada ho gaya !!")
+    print ("Jaa gaadi chala")
+
+elif (age == 0 ):
+    print ("bro is still in the black hole !")
+
+else : 
+    print ("bkl aukaat mein !")
