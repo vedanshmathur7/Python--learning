c8c92920d740a182341362217042187a6337a162
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:43:54 +0530

    Create if_else_.py

---
+++ chapter 6/if_else_.py
@@ -0,0 +1,10 @@
+age = int (input ("Enter your age : "))
+if (age >= 18):
+    print ("Baccha bada ho gaya !!")
+    print ("Jaa gaadi chala")
+
+elif (age == 0 ):
+    print ("bro is still in the black hole !")
+
+else : 
+    print ("bkl aukaat mein !")
