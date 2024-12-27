994b5bc80082e567d61e49d6938dc6e803aca133
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-27 11:44:09 +0530

    Create q4.py

---
+++ chapter 6/q4.py
@@ -0,0 +1,7 @@
+username = input("Enter your username : ")
+
+if (len(username) >= 10 ):
+    print("The username is containing 10<= characters. ")
+
+else : 
+    print("The username is containing 10> characters.")
