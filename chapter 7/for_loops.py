f94259762d51dd70e7c7f4f26713054bbd5aa46f
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-28 14:16:19 +0530

    Create for_loops.py

---
+++ chapter 7/for_loops.py
@@ -0,0 +1,23 @@
+# for i in range (78): #executes till (n-1)
+#     print(i) #prints till 77
+
+
+#For loops in list 
+list = [23, 546, "Vedansh"]
+
+for i in list:
+    print("Printing for lists", i)
+
+#For loops in tuples
+
+topi = (35, 67, 8567, "Vedansh")
+
+for i in topi:
+    print ("Printing for tuples", i)
+
+#For loops in Strings
+
+str = "Vedansh Mathur"
+
+for i in str :
+    print (i)
