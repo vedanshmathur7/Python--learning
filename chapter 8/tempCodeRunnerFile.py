cbcfae738cd80fde04bdceb5f4b6bdeea66b1077
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-12-30 22:51:33 +0530

    Create tempCodeRunnerFile.py

---
+++ chapter 8/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+def stripu(l, word):
+    for i in range (len(l)):
+        if word in l[i]:
+            l[i] = l[i].replace(word,"").strip()
+        return [item for item in l if item != ""]
+    
+l = ["Harry", "Rohan", "Mohan", "an"]
+
+print(stripu(l,"an"))
+
