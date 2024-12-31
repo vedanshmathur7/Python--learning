8b330e5c893ed2c1f4d4605cea84c75762d1d19a
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-01 01:56:07 +0530

    Create init_constructor.py

---
+++ chapter 10/init_constructor.py
@@ -0,0 +1,11 @@
+class Company:
+
+    def __init__(self, salary, language , department):
+        self.salary = salary
+        self.language = language
+        self.department = department
+
+vedansh = Company (12000, "Java", "Taxation")
+print (vedansh.salary, vedansh.language, vedansh.department)
+
+        
