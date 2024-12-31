2cf209230805533df6e7891d7e258e90e93e1faf
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-01 01:56:05 +0530

    Create class.py

---
+++ chapter 10/class.py
@@ -0,0 +1,11 @@
+class Employee :
+    language = "python" #class attributes
+    salary  = 1200000
+
+vedansh = Employee()
+vedansh.name = "Vedansh Mathur" #instance attributes
+print (vedansh.name, vedansh.language, vedansh.salary)
+
+joy = Employee()
+joy.name = "Chitranshu Mathur"
+print (joy.salary, joy.name)
