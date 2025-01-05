988f56463c9ddde64be27ad8b759385ac7060e98
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-05 12:25:09 +0530

    Create types.py

---
+++ chapter 12/types.py
@@ -0,0 +1,15 @@
+joy : int = 78
+
+#we have defined joy as an integer to access all the methods of integer type varible
+
+
+def sum (a : int , b : int ) -> int:
+    return a+b
+
+def greeting (name : str) -> str:
+    return f"Hello {name}"
+
+
+print (greeting("Vedansh"))
+print (sum (56, 78))
+
