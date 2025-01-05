af27326dde3ecd6828388ea146e683d22c701b80
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-05 12:25:07 +0530

    Create advanced_types.py

---
+++ chapter 12/advanced_types.py
@@ -0,0 +1,13 @@
+from typing import List, Union, Tuple, Dict
+
+numbers : List[str] = ["Vedansh", "Harry", "Tanish"]
+print (numbers)
+
+names : Tuple[str, int] = ("rajesh", "kamlesh" , 68, 89, "rajesh")
+print (names)
+
+chappal : Dict[str, int ]= {"harry" : 45, "bob": 90}
+print (chappal)
+
+identifier : Union[int, str] = "RAJESH1234"
+print (identifier)
