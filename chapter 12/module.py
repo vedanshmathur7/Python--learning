1275082ef449dcffefbb4eca359aca2523d48c55
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-06 19:18:40 +0530

    Create module.py

---
+++ chapter 12/module.py
@@ -0,0 +1,10 @@
+def myFunc():
+    print ("Hello World mf !! ")
+
+
+
+
+if (__name__ == "__main__" ):
+    print ("we directly accessing the main file !") 
+    myFunc()
+    print (__name__) #__main__ will be printed because it is opened in the main.py file
