18fe54af51b8adac03ff0ba9016ab525db5ab422
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-07 18:26:49 +0530

    Create q1.py

---
+++ chapter 12/q1.py
@@ -0,0 +1,25 @@
+try : 
+    with open ("file1.txt" ) as f:
+        print (f.read())
+
+except Exception as e:
+    print (e)
+
+
+try : 
+    with open ("file2.txt" ) as f:
+        print (f.read())
+
+except Exception as e:
+    print (e)
+
+
+try : 
+    with open ("file3.txt" ) as f:
+        print (f.read())
+
+except Exception as e:
+    print (e)
+
+
+print ("fuck u man , the code ain't crashed !")
