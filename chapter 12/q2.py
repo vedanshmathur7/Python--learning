8e10988fc73e12dca356f96493a19b20b454a4c6
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-07 18:26:52 +0530

    Create q2.py

---
+++ chapter 12/q2.py
@@ -0,0 +1,5 @@
+list = ["vedansh", "tejaswi", "vishal", "harshul", 56, 69, "tejaswii" ]
+
+for index, items in enumerate(list):
+    if (index == 2 or index == 4 or index == 6):
+        print (items) 
