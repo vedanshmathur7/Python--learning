cc9ac40aa24558a162e026c7e7615a43239af40e
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-09 00:21:08 +0530

    Create format.py

---
+++ chapter 13/format.py
@@ -0,0 +1,6 @@
+vedansh = "hello {} {} you all?".format("everyone","how" )
+#hello everyone everyone how you all?
+
+vedansh = "hello {1} {0} you all?".format("everyone","how" )
+# hello how everyone you all?
+print (vedansh)
