e62a1604f8a1c131ae98b9f2a248af2b8dd0d235
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-08 16:44:36 +0530

    Create try_else.py

---
+++ chapter 12/try_else.py
@@ -0,0 +1,10 @@
+try :
+    a = int (input ("Enter the no. : "))
+    print (a)
+
+except Exception as e :
+    print (e)
+
+else : 
+    print ("aaaaaoooo saaalooooo  !! I am inside the else !!")
+
