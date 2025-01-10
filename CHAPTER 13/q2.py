8a18b15082c4bf26701d2084fc74486b84afddde
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-10 10:09:45 +0530

    Create q2.py

---
+++ CHAPTER 13/q2.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name : ")
+marks = int (input ("Enter your marks : "))
+phno = int (input ("Enter your Phone no. : "))
+
+print ("Name of the person is {}, the marks that he has got is {}, and his phone no. is {}".format(name, marks, phno))
