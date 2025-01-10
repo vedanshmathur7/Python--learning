148b31465959fd8757ea8a41b3cad750c1805e5a
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-11 00:04:48 +0530

    Create main.py

---
+++ MEGA PROJECT 1 - Jarvis/main.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+import webbrowser
+import musicLibrary
+import requests # helps to handle taks from external source like chat gpt.
+from openai import OpenAI
+from gtts import gTTS # google text to speech
+import pygame #to play sound related tasks 
+
