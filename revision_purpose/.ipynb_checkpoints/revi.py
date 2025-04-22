6708a720fef312cd76caa51bd5ddac179cef0e47
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-04-23 00:53:46 +0530

    Update revi.py

--- revision_purpose/.ipynb_checkpoints/revi.py
+++ revision_purpose/.ipynb_checkpoints/revi.py
@@ -1,7 +1,10 @@
-dictu = {
-    "name" : "rajesh",
-    "roll no." : 240686,
-    "tejaswi" : "my love"
-}
-for i in dictu:
-    print(i)
+n = int(input("Enter the no.: "))
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print("The no. is not prime.")
+            break
+    else:
+        print("The no. is prime.")
+else:
+    print("The no. is not prime.")
