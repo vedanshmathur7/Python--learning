d19f70c2509538dd9232fbb61325902ceb42cefc
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-05-01 12:59:30 +0530

    Create mathutils.py

---
+++ revision_purpose/mathutils.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    return a+b
+
+def is_even(a):
+    if (a%2 == 0):
+        print ("The no. is even")
+
+    else :
+        print ("The no. is odd")
+        
+def fac(n):
+    if n==0 or n==1:
+        print (1)
+    else :
+        for i in range (n `):
+            n = n + (n-1)
+        print (num)
+    
