7baabf1e328f1f6fd253b66b287e6ca92d3dfe63
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-22 12:33:10 +0530

    Create Variables & Data Types.py

---
+++ revision_purpose/Python advance/Variables & Data Types.py
@@ -0,0 +1,9 @@
+# Write a Python program that takes two numbers as input from the user and prints their sum. Ensure the inputs are converted to appropriate data types.
+
+a = int (input ("Enter the no. 1 : "))
+b = int (input ("Enter the no. 2 : "))
+print (f'The sum of these two no.s is : {a+b}') 
+
+# Write a Python program to swap the values of two variables without using a temporary variable. 
+# For example, if a = 5 and b = 10, after swapping, a should be 10 and b should be 5.
+
