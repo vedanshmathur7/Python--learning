bcc61f843c0d724fc7c4ef18864b41f0829e356d
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-22 12:43:46 +0530

    Create sum.py

---
+++ revision_purpose/Python advance/Variables and data types/sum.py
@@ -0,0 +1,6 @@
+# Write a Python program that takes two numbers as input from the user and prints their sum. Ensure the inputs are converted to appropriate data types.
+
+a = int (input ("Enter the no. 1 : "))
+b = int (input ("Enter the no. 2 : "))
+print (f'The sum of these two no.s is : {a+b}') 
+
